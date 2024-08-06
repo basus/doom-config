5de1c324493ce2dd0071763a16113e039321ef3c
Shrutarshi Basu <shr@basus.me>
2024-08-06 23:41:20 +0530

    Add Catppuccin color theme

--- packages.el
+++ packages.el
@@ -48,3 +48,9 @@
 ;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
+
+(package! nginx-mode)
+
+;; Use the catpuccin theme in the Macchiato variant
+;;
+(package! catppuccin-theme)
