10b3fbd5badd7adb50c6e11cc3dca86339117df5
Shrutarshi Basu <shr@basus.me>
2025-06-13 13:14:17 -0400

    Change color theme to Gruvbox light

--- config.el
+++ config.el
@@ -24,7 +24,7 @@
 
 ;; NOTE: Getting symbols working required to install Nerd Fonts in Emacs by doing
 ;; M-x nerd-icons-install-fonts
-(setq doom-font (font-spec :family "Fira Code" :size 13 )
+(setq doom-font (font-spec :family "FiraCode Nerd Font Mono" :size 13 )
       doom-serif-font (font-spec :family "GoMono Nerd Font" :size 13)
       doom-symbol-font (font-spec :family "FiraCode Nerd Font Mono" :size 13 )
       doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
@@ -37,8 +37,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq catppuccin-flavor 'macchiato)
-(setq doom-theme 'catppuccin)
+(setq doom-theme 'doom-gruvbox-light)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
